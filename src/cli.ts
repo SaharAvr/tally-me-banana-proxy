c8c1e1b59dbcc9ce2c20a1bd07a1a7f4ae10f069
SaharAvr <64190984+SaharAvr@users.noreply.github.com>
2023-05-09 02:57:10 +0300

    [FIX] add space to server listen message

--- src/cli.ts
+++ src/cli.ts
@@ -7,5 +7,5 @@ const PORT_DEFAULT = 3000;
 const PORT = (portArg ? parseInt(portArg, 10) : PORT_DEFAULT);
 
 app.listen(PORT, () => {
-  console.log(`[Tally Me Banana Proxy]: ⚡⚡⚡listening on port ${PORT} ⚡⚡⚡`);
+  console.log(`[Tally Me Banana Proxy]: ⚡⚡⚡ listening on port ${PORT} ⚡⚡⚡`);
 });
