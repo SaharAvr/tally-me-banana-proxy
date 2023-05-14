3058427b8ba69ba2a6aa8c7952ddf3dfbf5ed8bc
SaharAvr <64190984+SaharAvr@users.noreply.github.com>
2023-05-14 19:41:41 +0300

    [REVERT] run tsc before launching bin script

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 /**
  * Tally Me Banana Proxy - A lightweight and powerful proxy server
  * @author Sahar Avraham
- * @version 4.0.2
+ * @version 4.0.3
  */
 
 import { startHttpServer } from './servers/http';
